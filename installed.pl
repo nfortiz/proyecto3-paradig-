96bbda514a12bf4e18585eb40e778fa70a78e905
nfortiz <nf.ortiz@uniandes.edu.co>
2019-05-25 19:51:29 -0500

    Inicial Commit

---
+++ installed.pl
@@ -0,0 +1,10 @@
+% installed(package_name, version)
+installed('p1', '1.2.3').
+installed('p2', '3.8.9').
+installed('p3', '2.1.7').
+
+%install-p/5.
+
+%install/5
+
+%depends(package, version, name, version, flag).

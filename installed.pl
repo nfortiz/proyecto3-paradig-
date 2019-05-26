4032055788cec4a7c0983855e94a4028a3d70395
nfortiz <nf.ortiz@uniandes.edu.co>
2019-05-25 20:15:20 -0500

    Añadiendo permisos para agregar clausulas al KB

--- installed.pl
+++ installed.pl
@@ -1,4 +1,5 @@
 % installed(package_name, version)
+:- dynamic installed/2.
 installed('p1', '1.2.3').
 installed('p2', '3.8.9').
 installed('p3', '2.1.7').
@@ -6,6 +7,8 @@ installed('p3', '2.1.7').
 %install-p/5(package_name, version).
 install-p().
 
-%install/5
+%install/5(package_name, version).
+install(package_name, version):- asserta(installed(package_name, version)).
+%install(package_name, version):- installed(package_name, version),!.
 
 %depends(package, version, name, version, flag).
